dff6426f5b1ada08d98cbdbd3f5b384ee56ee77b
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2022-05-10 21:28:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ let wktus = moment.tz('Asia/Jakarta').format('ss')
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
-global.owner = ['6289625556161','62831433937633', '62896255561617'] // Put your number here
+global.owner = ['6285878836361','6285878836361', '6285878836361'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -65,14 +65,14 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'ZEUSBOTZ-MD'
-global.author = `By Pilar`
-global.wm = 'ZEUS BOT - MD' 
+global.packname = 'Iky-MD'
+global.author = `Ikybot`
+global.wm = 'iky BOT - MD' 
 global.watermark = wm
-global.instagram = 'https://instagram.com/not_pilar32'
+global.instagram = 'https://instagram.com/ikykunnnn'
 global.botdate = `❏ ${wktuh} H  ${wktum} M  ${wktus} S\n❏ ${week} ${date}`
 global.media = 'https://telegra.ph/file/ccb2359d3925fd7af58cd.jpg'
-global.github = 'https://github.com/PilarV2'
+global.github = 'https://github.com/rizkiwibu'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*L O A D I N G*_'
 global.eror = '_*Server Error*_'
